3712717b19863ef131819b63a3c33ab808d7a2ca
sidhyaashu <ashutoshsidhya69@gmail.com>
2025-10-24 22:55:38 +0530

    x

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,12 @@
 // lib/prisma.ts
-// import { PrismaClient } from "@prisma/client";
 import { PrismaClient } from "@/lib/generated/prisma";
 
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-unused-vars
-  var prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
